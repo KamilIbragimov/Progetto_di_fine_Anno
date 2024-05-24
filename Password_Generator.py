e152c7fff7de3c788bfa5090f7b7a16aa26c6c66
KamilIbragimov <147493558+KamilIbragimov@users.noreply.github.com>
2024-05-24 21:18:56 +0200

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -3,41 +3,56 @@ import random
 import string
 import requests
 import re
+
+
+
 def password_generata() -> str:
+    """Genera una password casuale in base alla lunghezza inserita dall'utente.
+
+    Args: Nessuno.
+    Returns: Una password generata casualmente.
+    """
     try:
-        length = int(input("Inserisci la lunghezza desiderata per la password: "))
-        print("\n")
-        if length < 8 or length>20:
-            print("La lunghezza deve essere almeno di 8 caratteri e al massimo di 20 caratteri.")
+        while True:
+            length = int(input("Inserisci la lunghezza desiderata per la password: "))
             print("\n")
-            
-            password_generata()
-            print("\n")
-            
-            return password_generata
-        
-        # Caratteri disponibili per la password
-        characters = string.ascii_letters + string.digits + string.punctuation
-        
-        # Aggiungi almeno un carattere maiuscolo, uno minuscolo, un numero e un carattere speciale
-        password = random.choice(string.ascii_uppercase) + random.choice(string.ascii_lowercase) + random.choice(string.digits) + random.choice(string.punctuation)
-        
-        # Genera il resto della password casualmente
-        remaining_length = length - 4
-        password += ''.join(random.choice(characters) for _ in range(remaining_length))
-        
-        # Mescola la password per renderla più casuale
-        lista_password = list(password)
-        random.shuffle(lista_password)
-        password = ''.join(lista_password)
-        
-        return password
+            if length < 8 or length > 20:
+                print("La lunghezza deve essere almeno di 8 caratteri e al massimo di 20 caratteri.")
+                print("\n")
+                continue    
+
+            # Caratteri disponibili per la password
+            characters = string.ascii_letters + string.digits + string.punctuation
+
+            # Aggiungi almeno un carattere maiuscolo, uno minuscolo, un numero e un carattere speciale
+            password = random.choice(string.ascii_uppercase) + random.choice(string.ascii_lowercase) + random.choice(string.digits) + random.choice(string.punctuation)
+
+            # Genera il resto della password casualmente
+            remaining_length = length - 4
+            password += ''.join(random.choice(characters) for _ in range(remaining_length))
+
+            # Mescola la password per renderla più casuale
+            lista_password = list(password)
+            random.shuffle(lista_password)
+            password = ''.join(lista_password)
+
+            return password
     except ValueError:
         print("Errore: Inserisci un valore intero positivo per la lunghezza della password.")
         print("\n")
-        return ""
+        return password_generata()
+
+password_generata = password_generata()
+print(f"Password generata: {password_generata}")
+print("\n")
 
 def verifica_email() -> str:
+    """Questa funzione convalida un indirizzo email fornito dall’utente.
+
+
+    Args: nessuno.
+    return: indirizzo email valido.
+    """
     while True:
         try:
             indirizzo_email = input("Inserisci un indirizzo email: ")
@@ -51,20 +66,19 @@ def verifica_email() -> str:
             
             print("Inserisci un indirizzo email valido.")
             print("\n")
-            
-
-
-password_generata = password_generata()
 email_inserita = verifica_email()
 
 
+def verifica_sito() -> str:
+    """Verifica se un sito web inserito dall'utente esiste.
 
-
-
-def verifica_sito():
-    
+    Returns:
+        str: Il sito web esistente o una stringa vuota se non esiste.
+    """
+    while True:
         sito_collegato = input("Inserisci il sito collegato alle credenziali (es. google.com): ")
         print("\n")
+
         if re.match(r"^[a-zA-Z0-9.-]+$", sito_collegato):
             try:
                 response = requests.get(f"https://{sito_collegato}")
@@ -72,33 +86,24 @@ def verifica_sito():
                     print(f"Il sito {sito_collegato} esiste!")
                     print("\n")
                     return sito_collegato
-                    
-                    
-                
             except requests.ConnectionError:
-                print("Dominio mancante o sito inesistente")
-                print("\n")
-                verifica_sito()
+                print("Dominio mancante o sito inesistente!")
                 print("\n")
         else:
             print("Inserisci un sito valido (solo caratteri dell'alfabeto, punti e trattini).")
             print("\n")
-            verifica_sito()
-            print("\n")
 
-
-
-
-
-
-
-sito_inserito=verifica_sito()
+sito_inserito = verifica_sito()
 
 
 
 def crea_dizionario(email_inserita: str, password_generata: str,sito_inserito:str) -> dict:
     print("\n")
-    
+    """Questa funzione crea un dizionario contenente l’email dell’utente, la password e il sito web associato.
+
+    Args: password,email,sito.
+    return: Dizionario contenente le 3 chiavi.
+    """
     sito_inserito = f"{sito_inserito} "+" (esistente)"
     dizionario = {
         "password": password_generata,
@@ -108,17 +113,13 @@ def crea_dizionario(email_inserita: str, password_generata: str,sito_inserito:st
     return dizionario
 
 
+
 dati_dict = crea_dizionario(email_inserita, password_generata,sito_inserito)
-
-
-
 print("\n" * 80)
 
 
-
-
-print(f"Password email e sito associati")
-print("\n" * 1)
+print(f"Password email e sito associati:")
+print("\n")
 for chiave, valore in dati_dict.items():
     if chiave == "password":
         print(f"\033[91m{chiave}-->\033[0m{valore}")
@@ -126,4 +127,4 @@ for chiave, valore in dati_dict.items():
         print(f"\033[93m{chiave}-->\033[0m{valore}")
     else:
         print(f"\033[94m{chiave}-->\033[0m{valore}")
-    print("\n" * 1)
+    print("\n" )
